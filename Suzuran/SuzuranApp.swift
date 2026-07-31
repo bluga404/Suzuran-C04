da6fa1d04a7b64eaa3cc455ff5af2d55a8645f0d
Robby <robby396fs@gmail.com>
2026-07-31 10:01:49 +0700

    Initial Commit

---
+++ Suzuran/SuzuranApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuzuranApp.swift
+//  Suzuran
+//
+//  Created by Robby Yehezkiel Pardomuan on 31/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SuzuranApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
